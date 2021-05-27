d92f307b900d8fc1c10a61ab008763ba805c7402
ChillEspresso <dennisbagot1998@gmail.com>
2021-05-27 18:56:50 -0400

    First Init

---
+++ ProductWebsite/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+const hbs = require('express-handlebars');
+const path = require('path');
+
+app.use(express.json());
+
+//serving static files
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+
+//View Engine
+app.engine('hbs', hbs({
+    extname:'hbs',
+    defaultLayout: 'main',
+    layoutsDir: path.join(__dirname, 'views'),
+    partialsDir: path.join(__dirname, 'views/partials')
+}));
+app.set('view engine', 'hbs');
+
+//Calling routes
+app.use('/',require('./server/router/router'));
+
+app.listen(3000, ()=> console.log(`Server started on http://localhost:3000`));
