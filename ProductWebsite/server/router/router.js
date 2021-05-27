51f26e4ff5e834f524e2014fc1225326134a4fd8
ChillEspresso <dennisbagot1998@gmail.com>
2021-05-27 19:08:26 -0400

    add upload route

--- ProductWebsite/server/router/router.js
+++ ProductWebsite/server/router/router.js
@@ -1,7 +1,10 @@
 const route = require('express').Router()
 const controller = require('../controller/controller');
+const store = require('../middleware/multer');
 
 //routes
 route.get('/',controller.home);
+
+route.post('/uploadMultiple',store.array('images'));
 
 module.exports = route;
