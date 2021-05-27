5964059f5aaa8f4327cdb5d92b274efea5b295a0
ChillEspresso <dennisbagot1998@gmail.com>
2021-05-27 19:05:54 -0400

    add Storage variable

--- ProductWebsite/server/middleware/multer.js
+++ ProductWebsite/server/middleware/multer.js
@@ -1,14 +1,17 @@
 const multer=require('multer');
 
 //set storage
-multer.diskStorage({
+var Storage = multer.diskStorage({
     destination: function(req,file,cb){
         cb(null,'uploads')
     },
     filename: function(req,file,cb){
         //image.jpg
         var ext = file.originalname.substr(file.originalname.lastIndexOf('.'));
-        
+
         cb(null, file.filename+'-'+Date.now()+ ext)
     }
 })
+
+
+module.exports = store = multer({ storage: Storage})
