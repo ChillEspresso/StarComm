17041c22bcf554691523960a5e0a85a3087fa11b
ChillEspresso <dennisbagot1998@gmail.com>
2021-05-27 22:27:48 -0400

    upload img to mongo db

--- ProductWebsite/server/controller/controller.js
+++ ProductWebsite/server/controller/controller.js
@@ -1,3 +1,7 @@
+const UploadModel = require('../model/schema');
+const fs = require('fs');
+const { response } = require('express');
+
 exports.home=(req,res)=>{
     res.render('main');
 }
@@ -11,9 +15,46 @@ exports.uploads = (req,res,next)=>{
         return next(error)
     }
 
-    //conver images to base 64
-    res.json(files);
+    //conver images to base64 encoding 
 
+    let imgArray = files.map((file) =>{
+        let img = fs.readFileSync(file.path)
 
+        return encode_image = img.toString('base64')
+    })
+
+    let result = imgArray.map((src, index) =>{
+        //create object to store data in the database
+        let finalImage = {
+            filename:files[index].originalname,
+            contentType:files[index].mimetype,
+            imageBase64: src
+        }
+
+        let newUpload = new UploadModel(finalImage);
+
+        return newUpload
+            .save()
+            .then(() => {
+                return { msg: `${files[index].originalname}image Uploaded Successfullly`}
+            })
+            .catch(error =>{
+                if(error){
+                    if(error.name === 'MongoError' && error.code === 11000){
+                        return Promise.reject({ error: `Duplicate ${files[index].originalname}. File Already exists!`})
+                    }
+                    return Promise.reject({ error: error.message || `Cannot Uplaod ${files[index].originalname} Something Missing`})
+                }
+            })
+    });
+
+    Promise.all(result)
+        .then(msg =>{
+            response.json(msg);
+            //res.redirect('/')
+        })
+        .catch(err =>{
+            res.json(err)
+        })
 }
 
